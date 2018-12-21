03601828c91899ad50289b4ef0a70a3467a2c322
Justin Clift <justin@postgresql.org>
2018-12-21 22:55:33 +1100

    Initial dependency management using dep

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ import (
 	"net/http"
 	"net/url"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"time"
+
 	"github.com/BurntSushi/toml"
 	"github.com/jackc/pgx"
 	"github.com/jackc/pgx/pgtype"
@@ -18,9 +21,9 @@ import (
 
 const (
 	// Directories to load things from
-	baseDir = "/root/git_repos/db4s_cluster_downloader" // Location of the git source
+	baseDir = "/root/git_repos/db4s_cluster_downloader"          // Location of the git source
 	certDir = "/etc/letsencrypt/live/download.sqlitebrowser.org" // Location of the TLS certificates.  Shared with the host.
-	dataDir = "/root/data" // Directory where the downloads are located.  Shared with the host.
+	dataDir = "/root/data"                                       // Directory where the downloads are located.  Shared with the host.
 
 	// Application config settings
 	configFile = "/root/data/config.toml"
@@ -36,10 +39,9 @@ const (
 	DB4S_3_10_1_PORTABLE
 )
 
-
 // Configuration file
 type TomlConfig struct {
-	Pg        PGInfo
+	Pg PGInfo
 }
 type PGInfo struct {
 	Database       string
@@ -111,7 +113,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, filepath.Join(baseDir, "favicon.ico"))
 		logDownload(r, 90022, http.StatusOK) // 90022 is the file size of favicon.ico in bytes
 	case "/currentrelease":
-		bytesSent, err := fmt.Fprint(w,"3.10.1\nhttps://github.com/sqlitebrowser/sqlitebrowser/releases/tag/v3.10.1\n")
+		bytesSent, err := fmt.Fprint(w, "3.10.1\nhttps://github.com/sqlitebrowser/sqlitebrowser/releases/tag/v3.10.1\n")
 		if err != nil {
 			log.Printf("Error serving currentrelease: %v\n", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -151,27 +153,50 @@ func logDownload(r *http.Request, bytesSent int64, status int) (err error) {
 		ref.Status = pgtype.Null
 	}
 
-	// Try and extract more useful client IP info
-	// Note - written while half asleep. :/
-	clientIP := &pgtype.Text{
-		String: r.Header.Get("X-Forwarded-For"),
-		Status: pgtype.Present,
+	// Grab the client IP address
+	IPv6 := false
+	var clientIP pgtype.Text
+	var clientPort int
+	tempIP := r.Header.Get("X-Forwarded-For")
+	if tempIP == "" {
+		tempIP = r.RemoteAddr
 	}
-	if clientIP.String == "" {
-		clientIP.String = r.RemoteAddr
-		if clientIP.String == "" {
-			clientIP.Status = pgtype.Null
+	if tempIP != "" {
+		// Determine if client IP address is IPv4 or IPv6, and split out the port number
+		if strings.HasPrefix(tempIP, "[") {
+			IPv6 = true
+			s := strings.SplitN(tempIP, "]:", 2)
+			if s[1] != "" {
+				clientPort, _ = strconv.Atoi(s[1])
+			}
+			clientIP.String = strings.TrimLeft(s[0], "[")
+		} else {
+			// Client IP address seems to be IPv4
+			s := strings.SplitN(tempIP, ":", 2)
+			if s[1] != "" {
+				clientPort, _ = strconv.Atoi(s[1])
+			}
+			clientIP.String = s[0]
 		}
+	} else {
+		// Can't determine client IP address
+		clientIP.Status = pgtype.Null
 	}
 
-	dbQuery := `
-		INSERT INTO download_log (remote_addr, remote_user, request_time, request_type, request, protocol, status, body_bytes_sent, http_referer, http_user_agent)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
+fmt.Printf("ClientIP = '%v' Port = %d; IPv6 = %v\n", clientIP, clientPort, IPv6)
+
+	dbField := "client_ipv4"
+	if IPv6 {
+		dbField = "client_ipv6"
+	}
+
+	dbQuery := fmt.Sprintf("INSERT INTO download_log (%s, remote_user, request_time, request_type, request, protocol, status, body_bytes_sent, http_referer, http_user_agent, client_port)"+
+		"VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)", dbField)
 	res, err := pg.Exec(dbQuery,
-		// remote_addr
+		// client_ipv4 or client_ipv6
 		clientIP,
 		// remote_user
-		&pgtype.Text{String: "", Status: pgtype.Null,}, // Hard coded empty string for now
+		&pgtype.Text{String: "", Status: pgtype.Null}, // Hard coded empty string for now
 		// request_time
 		time.Now().Format(time.RFC3339Nano),
 		// request_type
@@ -187,7 +212,9 @@ func logDownload(r *http.Request, bytesSent int64, status int) (err error) {
 		// http_referer
 		ref,
 		// http_user_agent
-		r.Header.Get("User-Agent"))
+		r.Header.Get("User-Agent"),
+		// client port
+		clientPort)
 	if err != nil {
 		return err
 	}
@@ -257,7 +284,6 @@ func main() {
 	http.HandleFunc("/", handler)
 	fmt.Printf("Listening on port %d...\n", listenPort)
 	err = http.ListenAndServeTLS(fmt.Sprintf(":%d", listenPort), filepath.Join(certDir, "fullchain.pem"), filepath.Join(certDir, "privkey.pem"), nil)
-	//err = http.ListenAndServeTLS(fmt.Sprintf(":%d", listenPort), filepath.Join(certDir, "cert.pem"), filepath.Join(certDir, "privkey.pem"), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -290,13 +316,16 @@ func serveDownload(w http.ResponseWriter, r *http.Request, cacheEntry download, 
 		bytesSent, err := cacheEntry.reader.WriteTo(w)
 		if err != nil {
 			log.Printf("Error serving %s: %v\n", fileName, err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			// TODO: Catch when an error occurs (eg client aborts download), and log that too.  Probably need extra
+			//       fields added to the database for holding the info.
+			//logDownload(r, bytesSent, http.StatusBadRequest)
 			return
 		}
-		logDownload(r, bytesSent, http.StatusOK)
+		_ = logDownload(r, bytesSent, http.StatusOK)
 	} else {
 		// Warn the user
-		fmt.Fprintf(w, "Not yet available")
-		logDownload(r, 17, http.StatusNotFound)
+		_, _ = fmt.Fprintf(w, "Not yet available")
+		_ = logDownload(r, 17, http.StatusNotFound)
 	}
 }
